c882384c63d1b56d3244a5a598d0eb7b81339c6a
joekariuki3 <joekariuki3@gmail.com>
2025-08-25 15:08:48 +0300

    Set default Django settings module and add Redis backend configuration in Celery app initialization.

--- crm/celery.py
+++ crm/celery.py
@@ -1,4 +1,7 @@
 from celery import Celery
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj.settings')
 
 redis_url = 'redis://localhost:6379/0'
-celery_app = Celery('crm', broker=redis_url)
+celery_app = Celery('crm', broker=redis_url, backend=redis_url)
